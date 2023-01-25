9a759d21ef840b268c7e8c9db6d62f414d2da16e
Gioh Kim <gurugio@gmail.com>
2023-01-25 23:28:28 +0900

    my-channel: on going

--- my-channel/src/channels.rs
+++ my-channel/src/channels.rs
@@ -1,6 +1,19 @@
-use crate::semaphore::semaphore;
+use crate::semaphore::Semaphore;
 use std::collections::LinkedList;
 use std::sync::{Arc, Condvar, Mutex};
 
 #[derive(Clone)]
-pub struct 
+pub struct Sender<T> {
+    sem: Arc<Semaphore>,
+    buf: Arc<Mutex<LinkedList<T>>>,
+    cond: Arc<Condvar>,
+}
+
+impl<T: Send> Sender<T> {
+    pub fn send(&self, data: T) {
+        self.sem.wait();
+        let mut buf = self.buf.lock().unwrap();
+        buf.push_back(data);
+        self.cond.notify_one();
+    }
+}
--- my-channel/src/main.rs
+++ my-channel/src/main.rs
@@ -1,3 +1,4 @@
+mod channels;
 mod semaphore;
 
 fn main() {
